91df64a0cda435b601f9e22a9ed142aed08f1ba3
JiSoo Kim <41679458+ghis22130@users.noreply.github.com>
2020-09-16 15:12:36 +0900

    Update ViewController.swift

--- MyFirstSwiftProject/ViewController.swift
+++ MyFirstSwiftProject/ViewController.swift
@@ -8,14 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+    var pictures = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         
+        let fm = FileManager.default    //파일을 찾는데 사용
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)   //파일 이름, try로 접근 실패시 아무것도 안일어나게
+        
+        for item in items {
+            if item.hasPrefix("nssl"){  //접두사
+                //this is a picture to load!
+                pictures.append(item)
+            }
+        }
+        print(pictures)
     }
-    
-    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
 }
 
